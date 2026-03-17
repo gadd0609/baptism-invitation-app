17881736f0a6c36b31588341218a3b9ffa3797d5
gaddiel.patino <gaddiel.patino@cimagroup.com.mx>
2026-03-17 01:55:09 -0600

    Fix(): Add correct number

--- src/components/info/hook.js
+++ src/components/info/hook.js
@@ -1,7 +1,7 @@
 export default function useInfoComponent() {
   
-  const phoneNumber = '3121051182' 
-  const defaultMessage = 'Hola, vengo desde tu invitación. Confirmo asistencia con 2 personas.'
+  const phoneNumber = '3125937414' 
+  const defaultMessage = 'Hola, vengo desde tu invitación. Confirmo asistencia con (cantidad de personas) personas.'
   const whatsappUrl = `https://wa.me/${phoneNumber}?text=${defaultMessage}`
   
   return {
