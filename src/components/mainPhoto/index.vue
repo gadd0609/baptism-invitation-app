f74c78af435b4dcd6c1e27e1e1aaf840d3f11e0e
gaddiel.patino <gaddiel.patino@cimagroup.com.mx>
2026-03-18 13:23:22 -0600

    Fix(): change main photo

--- src/components/mainPhoto/index.vue
+++ src/components/mainPhoto/index.vue
@@ -2,7 +2,7 @@
     <div class="overflow-hidden sm:pts-16 text-center rounded-3xl shadow-[0_8px_40px_8px_rgba(0,0,0,0.25)]  bg-pearl">
       <div class="relative mx-auto  rounded-t-3xl">
         <img 
-          src="/photos/photoGallery04.jpg" 
+          src="/photos/photoMain.jpg" 
           alt="photo.alt"
           class="w-full object-cover overflow-hidden sm:rounded-2xl rounded-t-3xl"
         />
